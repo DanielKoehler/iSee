f3c1231e2631c8e8ee241e538d43a7df130c95bd
Daniel Koehler <dandanarno@gmail.com>
2014-01-25 15:25:56 +0000

    Initial Commit

---
+++ iSee/LandingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingViewController.h
+//  iSee
+//
+//  Created by Daniel Koehler on 25/01/2014.
+//  Copyright (c) 2014 Daniel Koehler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LandingViewController : NSObject
+
+@end
---
+++ iSee/LandingViewController.m
@@ -0,0 +1,13 @@
+//
+//  LandingViewController.m
+//  iSee
+//
+//  Created by Daniel Koehler on 25/01/2014.
+//  Copyright (c) 2014 Daniel Koehler. All rights reserved.
+//
+
+#import "LandingViewController.h"
+
+@implementation LandingViewController
+
+@end
