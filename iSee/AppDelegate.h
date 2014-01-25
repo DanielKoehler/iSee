f1a1ed0c6fc68ac8a61834f88343d5e23beb18f2
Daniel Koehler <dandanarno@gmail.com>
2014-01-25 15:17:18 +0000

    Initial Commit

---
+++ iSee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSee
+//
+//  Created by Daniel Koehler on 25/01/2014.
+//  Copyright (c) 2014 Daniel Koehler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iSee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iSee
+//
+//  Created by Daniel Koehler on 25/01/2014.
+//  Copyright (c) 2014 Daniel Koehler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
